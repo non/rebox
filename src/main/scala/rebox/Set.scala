c821b3a1fda106d362954d651e031ef277cb73fa
Erik Osheim <d_m@plastic-idolatry.com>
2014-07-14 22:44:08 -0400

    Add equality, hashCode, and some tests for Set.

--- src/main/scala/rebox/Set.scala
+++ src/main/scala/rebox/Set.scala
@@ -12,25 +12,44 @@ object Set {
 
   def apply[A](base: debox.Set[A]): Set[A] =
     new Set[A](im.Set.empty, im.Set.empty, base)
+
+  def fromIterable[A](as: Iterable[A])(implicit ct: ClassTag[A]): Set[A] =
+    apply(debox.Set.fromIterable(as))
 }
 
-class Set[A] private[rebox] (added: im.Set[A], removed: im.Set[A], base: debox.Set[A]) extends Iterable[A] {
+class Set[A] private[rebox] (added: im.Set[A], removed: im.Set[A], private[rebox] val base: debox.Set[A]) extends Iterable[A] {
+
+  override def hashCode: Int =
+    foldLeft(0xabe2daeb)(_ ^ _.##)
+
+  override def equals(that: Any): Boolean =
+    that match {
+      case r: rebox.Set[_] =>
+        if (r.size != size || r.base.ct != base.ct) return false
+        val rr = r.asInstanceOf[Set[A]]
+        rr.forall(this.apply)
+      case _ =>
+        false
+    }
 
   def apply(a: A): Boolean =
     added(a) || base(a) && !removed(a)
 
-  def += (a: A): Set[A] =
+  def + (a: A): Set[A] =
     if (added(a)) this
     else if (base(a)) new Set(added, removed - a, base)
     else new Set(added + a, removed - a, base)
 
-  def -= (a: A): Set[A] =
+  def - (a: A): Set[A] =
     if (base(a)) new Set(added - a, removed + a, base)
     else if (added(a)) new Set(added - a, removed, base)
     else this
 
   override def size: Int =
     added.size - removed.size + base.size
+
+  def modSize: Int =
+    added.size + removed.size
 
   def compact: Set[A] =
     Set(toDebox)
---
+++ src/test/scala/rebox/SetCheck.scala
@@ -0,0 +1,101 @@
+package rebox
+
+import org.scalatest.matchers.ShouldMatchers
+import org.scalatest._
+import prop._
+import org.scalacheck.Arbitrary._
+import org.scalacheck._
+import Gen._
+import Arbitrary.arbitrary
+
+import spire.algebra.Order
+import spire.std.any._
+
+import scala.collection.{immutable => im}
+import scala.reflect.ClassTag
+
+abstract class SetCheck[A: Arbitrary: ClassTag: Order]
+    extends PropSpec with Matchers with GeneratorDrivenPropertyChecks {
+
+  def adhoc[A: ClassTag](as: Iterable[A]): rebox.Set[A] =
+    as.foldLeft(rebox.Set.empty[A])(_ + _)
+
+  property("from empty") {
+    forAll { xs: im.Set[A] =>
+      val r = adhoc(xs)
+      r.foreach { x => xs(x) shouldBe true }
+      xs.foreach { x => r(x) shouldBe true }
+    }
+  }
+
+  property("from debox") {
+    forAll { xs: im.Set[A] =>
+      val d = debox.Set.fromIterable(xs)
+      val r = rebox.Set(d)
+      r.foreach { x => d(x) shouldBe true }
+      d.foreach { x => r(x) shouldBe true }
+    }
+  }
+
+  property("partial build") {
+    forAll { (xs: im.Set[A], ys: im.Set[A]) =>
+      val r1 = adhoc(xs | ys)
+      val r2 = ys.foldLeft(rebox.Set(debox.Set.fromIterable(xs)))(_ + _)
+      val r3 = rebox.Set(debox.Set.fromIterable(xs | ys))
+      r1 shouldBe r2
+      r2 shouldBe r3
+    }
+  }
+
+  property("equals") {
+    forAll { xs: im.Set[A] =>
+      val r1 = adhoc(xs)
+      val r2 = rebox.Set(debox.Set.fromIterable(xs))
+      r1 shouldBe r2
+      r2 shouldBe r1
+    }
+  }
+
+  property("add (+) and remove (-)") {
+    forAll { (xs: im.Set[A], m: List[(A, Boolean)]) =>
+      val r1 = m.foldLeft(rebox.Set.fromIterable(xs)) { case (r, (k, b)) =>
+        if (b) r + k else r - k
+      }
+      val r2 = rebox.Set.fromIterable(m.foldLeft(xs) { case (s, (k, b)) =>
+        if (b) s + k else s - k
+      })
+      r1 shouldBe r2
+      r1.size shouldBe r2.size
+    }
+  }
+
+  property("toScala") {
+    forAll { xs: im.Set[A] =>
+      val r = adhoc(xs)
+      r.toScala shouldBe xs
+    }
+  }
+
+  property("toDebox") {
+    forAll { xs: im.Set[A] =>
+      val d = debox.Set.fromIterable(xs)
+      val r = adhoc(xs)
+      r.toDebox shouldBe d
+    }
+  }
+
+  property("compact") {
+    forAll { xs: im.Set[A] =>
+      val r1 = adhoc(xs)
+      val r2 = r1.compact
+      r2 shouldBe r1
+      r1.modSize shouldBe r1.size
+      r2.modSize shouldBe 0
+    }
+  }
+}
+
+class BooleanSetCheck extends SetCheck[Boolean]
+class IntSetCheck extends SetCheck[Int]
+class DoubleSetCheck extends SetCheck[Double]
+class StringSetCheck extends SetCheck[String]
